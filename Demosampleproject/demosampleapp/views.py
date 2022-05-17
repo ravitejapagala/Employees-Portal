3f46ec74fca0d1fac9be478283cb88206a33128a
ravitejapagala <84474944+ravitejapagala@users.noreply.github.com>
2022-05-17 15:55:42 +0530

    Update views.py

--- Demosampleproject/demosampleapp/views.py
+++ Demosampleproject/demosampleapp/views.py
@@ -18,7 +18,7 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("demosampleapp:homepage")
+			return redirect("demosampleapp:fileupload")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="register.html", context={"register_form":form})
